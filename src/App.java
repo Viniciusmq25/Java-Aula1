bcf7c3d1e443bb2a902febe91e161e2bb2098dbd
Vinicius <viniciusquintian@gmail.com>
2024-03-07 14:21:15 -0300

    aula 2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,24 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindo ao Screen Match");
-        System.out.println("Filme: Deadpool 3");
-
+    public static void main(String args[]) {
         int ano = 2022;
+        boolean incluidoNoPlano = true;
+        double media = (9 + 9 + 10 + 10) / 4;
+        String sinopse;
+        sinopse = """
+
+          Deadpool is a 2018 American superhero film 
+        based on the Marvel Comics character of the same name.
+        it has the scored 9.8 on the IMDB.
+         """;
+         double classificacao;
+         classificacao = (double) (media /2);
+
+        System.out.println("Bem vindo ao Screen Match!");
+        System.out.println("Filme: Deadpool");
+        System.out.println(ano);
+        System.out.println("nota dividida por 2: " + classificacao);
+        System.out.println(incluidoNoPlano);
+        System.out.println(sinopse);
 
     }
 }
