d3fa2afb3a7ea326b3e94da94fb3f7024fb620f3
Vinicius <viniciusquintian@gmail.com>
2024-03-11 14:45:12 -0300

    loop

---
+++ src/Loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Loop {
+  public static void main(String[] args) {
+    Scanner leitura = new Scanner(System.in);
+    double nota, media = 0;
+
+    for (int i = 0; i < 3; i++) {
+      System.out.println("Qual a nota do filme?");
+      nota = leitura.nextDouble();
+      media += nota;
+    }
+
+    System.out.println("media de avaliacoes: " + media / 3);
+
+  }
+}
--- src/leitura.java
+++ src/leitura.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
-public class leitura {
+public class Leitura {
   public static void main(String[] args) {
     Scanner leitura = new Scanner(System.in);
-  }
+
+    System.out.println("digite seu filme favorito");
+    String filme = leitura.nextLine();
+
+    System.out.println("Qual o ano de lançamento?: ");
+    int ano = leitura.nextInt();
+  
+    System.out.println("Qual a nota do filme?: ");
+    double nota = leitura.nextDouble();
+
+    System.out.println("seu filme preferito é " + filme + ", de ano " + ano + " e voce avaliou com " + nota);
+
+    leitura.close();
+  } 
 }
